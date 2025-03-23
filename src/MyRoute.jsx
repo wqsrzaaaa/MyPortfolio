9133038c71b1480b20cea8ed8c8ed69528e709e3
Waqas Raza <wqsrza07@gmail.com>
2025-03-23 16:03:24 +0500

    Updated project with latest changes

--- src/MyRoute.jsx
+++ src/MyRoute.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Link, NavLink  } from "react-router-dom";
 import { CiHome } from "react-icons/ci";
 import { IoCallOutline } from "react-icons/io5";
-
+import skill from './assets/skill.jpg'
 
 const MyRoute = () => {
   return (
@@ -10,25 +10,25 @@ const MyRoute = () => {
    <ul className="flex Several-Links gap-5">
       <li>
         <NavLink className="Link flex" to="/home">
-          <CiHome className="md:hidden block text-2xl" />
+          <CiHome  id="img"  className="md:hidden !scale-200 block text-2xl" />
           <span className="md:block hidden">About</span>
         </NavLink>
       </li>
       <li>
         <NavLink className="Link flex" to="/skills">
-          <img src="data:image/png;base64,...yourImage" className="md:hidden w-[25px] block text-2xl" />
+          <img  id="img" src={skill} className="md:hidden w-[25px] block text-2xl" />
           <span className="md:block hidden">Skills</span>
         </NavLink>
       </li>
       <li>
         <NavLink className="Link flex" to="/work">
-          <img src="https://cdn-icons-png.flaticon.com/512/2593/2593066.png" className="md:hidden w-[25px] block text-2xl" />
+          <img id="img"  src="https://cdn-icons-png.flaticon.com/512/2593/2593066.png" className="md:hidden w-[25px] block text-2xl" />
           <span className="md:block hidden">Work</span>
         </NavLink>
       </li>
       <li>
       <NavLink className="Link flex" to="/contact">
-        <IoCallOutline className="md:hidden block text-2xl" />
+        <IoCallOutline id="img"  className="md:hidden  !scale-150  block text-2xl" />
         <span className="md:block hidden">Contact</span>
       </NavLink>
       </li>
